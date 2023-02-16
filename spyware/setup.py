3ec94232247f98d1e71d0efddb7d953abc10a611
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-15 21:48:02 -0300

    Add the official OpenAPI API to use chatGPT

--- spyware/setup.py
+++ spyware/setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["os"], "includes": ["keyboard", "psutil", "requests", "getmac", "PIL", "threading", "IPy", "socket", "time", "scapy.all", "scapy"]}
+build_exe_options = {"packages": ["os"], "includes": ["keyboard", "psutil", "requests", "getmac", "PIL", "threading", "IPy", "socket", "time", "scapy.all", "scapy", "openai"]}
 
 base = None
 
